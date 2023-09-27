acc26d36d0554a1e3905c32d5bf8d5a2494aea51
TurtlePult <ivan_ladiygin@mail.ru>
2023-09-27 20:03:11 +0500

    perhabs a working programm has been craeted

---
+++ semka3.java
@@ -0,0 +1,20 @@
+import java.io.FileNotFoundException;
+
+public class semka3 {
+    public static void main(String[] args) throws Exception {
+   try {
+       int a = 90;
+       int b = 3;
+       System.out.println(a / b);
+       printSum(23, 234);
+       int[] abc = { 1, 2 };
+       abc[3] = 9;
+   } catch (IndexOutOfBoundsException ex) {
+       System.out.println("Массив выходит за пределы своего размера!");
+   }
+}
+public static void printSum(Integer a, Integer b) throws FileNotFoundException {
+   System.out.println(a + b);
+}
+
+}
