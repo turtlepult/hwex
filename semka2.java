5d4ca24d358a586da6b31ceabd6acfa5c3a5cebf
TurtlePult <ivan_ladiygin@mail.ru>
2023-09-27 19:52:56 +0500

    perhabs a working programm has been craeted

---
+++ semka2.java
@@ -0,0 +1,14 @@
+public class semka2 {
+    public static void main(String[] args) {
+        try {
+        int [] intArray = new int [] {0,0,0,0,0,0,0,0,10};
+        int d = 0;
+        double catchedRes1 = intArray[8] / d;
+        System.out.println("catchedRes1 = " + catchedRes1);
+     } catch (ArithmeticException e) {
+        System.out.println("Catching exception: " + e);
+     }
+    
+    }
+    
+}
